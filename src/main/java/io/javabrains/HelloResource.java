5d2f9381ecd9141751cfd3b051676392560fd264
= <khangie@hotmail.com>
2020-12-06 16:36:42 -0800

    Added /authentication endpoint

--- src/main/java/io/javabrains/HelloResource.java
+++ src/main/java/io/javabrains/HelloResource.java
@@ -1,16 +1,58 @@
 package io.javabrains;
 
+import io.javabrains.models.AuthenticationRequest;
+import io.javabrains.models.AuthenticationResponse;
+import io.javabrains.services.MyUserDetailsService;
+import io.javabrains.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloResource {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
 
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World";
     }
+
+    // This endpoint is open so it does not require authentication as with /hello.  See configure() method in SecurityConfigurer
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+        // Authenticate with username and password
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+
+        // Get user details from request
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+        // Generate JWT token
+        final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+        // Return a 200 ok with the payload of AuthenticationResponse model
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+
+    }
+
 
 }
--- src/main/java/io/javabrains/SecurityConfigurer.java
+++ src/main/java/io/javabrains/SecurityConfigurer.java
@@ -3,7 +3,9 @@ package io.javabrains;
 import io.javabrains.services.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -20,9 +22,23 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(myUserDetailsService);
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/authenticate").permitAll() // allows access to this endpoint without the need to authenticate
+                .anyRequest().authenticated();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    // For compatibility with newer versions of Spring.  This used to be the default bean but doesn't exist anymore.
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
 }
